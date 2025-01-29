122a09680c2c1895863065cce6d6ffdb3924a4b9
MarkuwM <silvaalderlindson@gmail.com>
2025-01-28 21:41:29 -0300

    1

---
+++ cadastro.js
@@ -0,0 +1,14 @@
+let nome = document.getElementById('nome')
+let email = document.getElementById('email')
+let telefone = document.getElementById('telefone')
+let celular = document.getElementById('celular')
+let salvar = document.getElementById('salvar')
+let limpar = document.getElementById('limpar')
+
+salvar.addEventListener('click', function(){
+    if(nome = null){
+        alert('Preencha todos os campos')
+    }
+    alert('Cadastro realizado com sucesso')
+}
+)
---
+++ codigo.js
@@ -0,0 +1,5 @@
+let usuario = prompt('Qual seu nome?')
+alert(usuario)
+
+let nomeFulano = document.getElementById('nomeFulano')
+nomeFulano.innerText = usuario
