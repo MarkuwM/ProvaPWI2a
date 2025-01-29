6e3fae449f55dbdedc96dd17d7e9346034d29984
MarkuwM <silvaalderlindson@gmail.com>
2025-01-28 21:52:15 -0300

    2

--- cadastro.js
+++ cadastro.js
@@ -5,10 +5,24 @@ let celular = document.getElementById('celular')
 let salvar = document.getElementById('salvar')
 let limpar = document.getElementById('limpar')
 
-salvar.addEventListener('click', function(){
+function campos(){
     if(nome = null){
         alert('Preencha todos os campos')
+    }else if(email = null){
+        alert('Preencha todos os campos')
+    }else if(telefone = null){
+        alert('Preencha todos os campos')
+    }else if(celular = null){
+        alert('Preencha todos os campos')
+    }else{
+        alert('Cadastro realizado com sucesso')
     }
-    alert('Cadastro realizado com sucesso')
 }
-)
+
+salvar.addEventListener('click', campos)
+limpar.addEventListener('click', function(){
+    nome = document.getElementById('nome').value = ' '
+    email = document.getElementById('email').value = ' '
+    telefone = document.getElementById('telefone').value = ' '
+    celular = document.getElementById('celular').value = ' '
+})
